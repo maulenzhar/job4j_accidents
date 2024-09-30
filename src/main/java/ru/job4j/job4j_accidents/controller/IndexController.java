e677e1146cb17c39209ed1efe9de806ea4aada70
Maulen Zharylgasynov <exmpl@gmail.com>
2024-10-01 01:13:59 +0600

    Загрузка данных в вид

--- src/main/java/ru/job4j/job4j_accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/job4j_accidents/controller/IndexController.java
@@ -2,13 +2,13 @@ package ru.job4j.job4j_accidents.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class IndexController {
-
-    @GetMapping
-    public String getIndex() {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("user", "Petr Arsentev");
         return "index";
     }
-
 }

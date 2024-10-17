ad50328b58d4a19189b882430c6d6135e8986677
Maulen Zharylgasynov <exmpl@gmail.com>
2024-10-17 08:27:49 +0600

    Загрузка данных в вид

--- src/main/java/ru/job4j/accidents/Job4jAccidentsApplication.java
+++ src/main/java/ru/job4j/accidents/Job4jAccidentsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Job4jAccidentsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(Job4jAccidentsApplication.class, args);
     }
-
 }

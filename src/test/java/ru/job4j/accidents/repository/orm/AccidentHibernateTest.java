fac4f3f53df43642526b30d341400149c4625549
Maulen Zharylgasynov <exmpl@gmail.com>
2025-02-18 04:33:21 +0600

    Spring ORM

--- src/test/java/ru/job4j/accidents/repository/orm/AccidentHibernateTest.java
+++ src/test/java/ru/job4j/accidents/repository/orm/AccidentHibernateTest.java
@@ -2,6 +2,7 @@ package ru.job4j.accidents.repository.orm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import ru.job4j.accidents.model.AccidentType;
@@ -19,6 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class AccidentHibernateTest {
 
     @Autowired

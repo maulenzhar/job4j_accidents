2ac0627410e936d76c818d515dcd2ae598d17eb1
Maulen Zharylgasynov <exmpl@gmail.com>
2025-02-19 02:15:56 +0600

    Spring ORM

--- src/main/java/ru/job4j/accidents/config/HbmConfig.java
+++ src/main/java/ru/job4j/accidents/config/HbmConfig.java
@@ -14,7 +14,7 @@ import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
-@PropertySource("classpath:db-${spring.profiles.active}.properties")
+@PropertySource("classpath:db-test.properties")
 @EnableTransactionManagement
 public class HbmConfig {
 
